ffb359aa6ed0cfec0978b1a7157af5ca0687c5ad
docktermj <michael@dockter.com>
2023-08-31 15:25:40 -0400

    #91 Additional tests

--- g2engineconfigurationjson/g2engineconfigurationjson_windows_test.go
+++ g2engineconfigurationjson/g2engineconfigurationjson_windows_test.go
@@ -4,23 +4,23 @@ package g2engineconfigurationjson
 
 var testCasesForOsArch = []testCaseMetadata{
 	{
-		name:         "sqlite3-001",
-		databaseUrl:  "sqlite3://na:na@nowhere/C:\\Temp\\sqlite\\G2C.db",
-		databaseFile: "C:\\Temp\\sqlite\\G2C.db",
+		name:            "sqlite3-001",
+		databaseUrl:     "sqlite3://na:na@nowhere/C:\\Temp\\sqlite\\G2C.db",
+		databaseUrlPath: "C:\\Temp\\sqlite\\G2C.db",
 	},
 	{
-		name:         "sqlite3-002",
-		databaseUrl:  "sqlite3://na:na@nowhere/C:\\Temp\\sqlite\\G2C.db",
-		databaseFile: `C:\Temp\sqlite\G2C.db`,
+		name:            "sqlite3-002",
+		databaseUrl:     "sqlite3://na:na@nowhere/C:\\Temp\\sqlite\\G2C.db",
+		databaseUrlPath: `C:\Temp\sqlite\G2C.db`,
 	},
 	{
-		name:         "sqlite3-003",
-		databaseUrl:  `sqlite3://na:na@nowhere/C:\Temp\sqlite\G2C.db`,
-		databaseFile: "C:\\Temp\\sqlite\\G2C.db",
+		name:            "sqlite3-003",
+		databaseUrl:     `sqlite3://na:na@nowhere/C:\Temp\sqlite\G2C.db`,
+		databaseUrlPath: "C:\\Temp\\sqlite\\G2C.db",
 	},
 	{
-		name:         "sqlite3-004",
-		databaseUrl:  `sqlite3://na:na@nowhere/C:\Temp\sqlite\G2C.db`,
-		databaseFile: `C:\Temp\sqlite\G2C.db`,
+		name:            "sqlite3-004",
+		databaseUrl:     `sqlite3://na:na@nowhere/C:\Temp\sqlite\G2C.db`,
+		databaseUrlPath: `C:\Temp\sqlite\G2C.db`,
 	},
 }
