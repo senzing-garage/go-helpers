c1895fb44c307ed501fbe019231697faaaadd415
Michael Dockter <michael@dockter.com>
2023-10-18 10:49:00 -0700

    97 Added FixtureRecords (#98)
    * 97 Added FixtureRecords
    
    * 97 Added FixtureRecords
    
    * 97 Added FixtureRecords
    
    * 97 Added FixtureRecords

---
+++ testfixtures/fixturerecords.go
@@ -0,0 +1,19 @@
+package testfixtures
+
+import (
+	"strings"
+
+	"github.com/senzing/go-common/record"
+)
+
+// ----------------------------------------------------------------------------
+// Variables
+// ----------------------------------------------------------------------------
+
+var FixtureRecords = map[string]record.Record{
+	"65536-periods": {
+		DataSource: "TEST",
+		Id:         "65536-periods",
+		Json:       `{"DATA_SOURCE":"TEST","RECORD_ID":"65536-periods","NAME_FULL":"Nobody Really","PERIODS":"` + strings.Repeat(".", 65536) + `"}`,
+	},
+}
