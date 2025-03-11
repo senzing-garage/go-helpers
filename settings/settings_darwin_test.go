ecc56e46389366d1683aaab23e653959ffe2f68b
docktermj <michael@dockter.com>
2025-03-11 10:37:53 -0400

    #143 Fix darwin/windows

--- settings/settings_darwin_test.go
+++ settings/settings_darwin_test.go
@@ -13,14 +13,12 @@ var testCasesForOsArch = []testCaseMetadata{
 		databaseURL:     "sqlite3://na:na@/var/opt/senzing/sqlite/G2C.db",
 		databaseURLPath: "/var/opt/senzing/sqlite/G2C.db",
 		databaseURI:     "sqlite3://na:na@/var/opt/senzing/sqlite/G2C.db",
-		isReversible:    true,
 	},
 	{
 		name:            "sqlite3-002",
 		databaseURL:     `sqlite3://na:na@hostname/var/opt/senzing/sqlite/G2C.db`,
 		databaseURLPath: "/var/opt/senzing/sqlite/G2C.db",
 		databaseURI:     `sqlite3://na:na@hostname/var/opt/senzing/sqlite/G2C.db`,
-		isReversible:    true,
 	},
 }
 
