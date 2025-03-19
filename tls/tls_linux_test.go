cffa9a8824a29a6bf526dab69126287aa956c46d
docktermj <michael@dockter.com>
2025-03-19 17:22:27 -0400

    #148 Fix platform tests

--- tls/tls_linux_test.go
+++ tls/tls_linux_test.go
@@ -12,10 +12,10 @@ import (
 // Test Public functions
 // ----------------------------------------------------------------------------
 
-func TestLoadX509KeyPair_unencrypted(test *testing.T) {
+func TestLoadX509KeyPair_encrypted(test *testing.T) {
 	certFile := "../testdata/certificates/client/certificate.pem"
-	keyFile := "../testdata/certificates/client/private_key.pem"
-	password := ""
+	keyFile := "../testdata/certificates/client/private_key_encrypted.pem"
+	password := "Passw0rd"
 	_, err := LoadX509KeyPair(certFile, keyFile, password)
 	require.NoError(test, err)
 }
