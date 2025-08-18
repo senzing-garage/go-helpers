c8bc28143f29ffd7e83a55fc1c3276c3b5429f37
Michael Dockter <michael@dockter.com>
2025-08-18 18:17:07 -0400

    Fix lint issues

--- tls/main.go
+++ tls/main.go
@@ -1,2 +1,6 @@
 // Package tls helps with Transport Layer Security.
 package tls
+
+import "errors"
+
+var errForPackage = errors.New("tls") //nolint
--- tls/tls_darwin.go
+++ tls/tls_darwin.go
@@ -6,6 +6,8 @@ import (
 	"context"
 	"crypto/tls"
 	"fmt"
+
+	"github.com/senzing-garage/go-helpers/wraperror"
 )
 
 func loadX509KeyPairWithPassword(
@@ -19,5 +21,6 @@ func loadX509KeyPairWithPassword(
 	_ = keyFile
 	_ = password
 	var tlsCertificate tls.Certificate
-	return tlsCertificate, fmt.Errorf("cannot decript %s on macOS platform", keyFile)
+
+	return tlsCertificate, wraperror.Errorf(errForPackage, fmt.Sprintf("cannot decript %s on macOS platform", keyFile))
 }
