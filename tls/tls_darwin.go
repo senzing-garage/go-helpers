9eb5bb5c536603f02e1a6a891fb6c783eb4c26df
Michael Dockter <michael@dockter.com>
2025-08-18 18:01:42 -0400

    Fix issue with ctx

--- tls/tls_darwin.go
+++ tls/tls_darwin.go
@@ -3,11 +3,18 @@
 package tls
 
 import (
+	"context"
 	"crypto/tls"
 	"fmt"
 )
 
-func loadX509KeyPairWithPassword(certFile string, keyFile string, password string) (tls.Certificate, error) {
+func loadX509KeyPairWithPassword(
+	ctx context.Context,
+	certFile string,
+	keyFile string,
+	password string,
+) (tls.Certificate, error) {
+	_ = ctx
 	_ = certFile
 	_ = keyFile
 	_ = password
